5aee8bbd847fdbb630000ec563c0062fe056a0ac
you92524 <57396776+you92524@users.noreply.github.com>
2019-12-02 22:35:28 +0800

    完成  欄位 & 攻擊事件

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    public float HP = 50;
+    [Header("攻擊力")]
+    public float Atk = 20f;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip Sound_Atk;
+    [Header("殭屍")]
+    public GameObject ZB;
+
+    private void attack()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("玩家攻擊");
+            print("玩家攻擊力:" + Random.Range(0f, Atk));
+        }
+        
+
+    }
+
+    private void Playerhurt()
+    {
+        
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attack();
+    }
+}
---
+++ UnityProject/Assets/Zombie.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+
+
+    [Header("血量")]
+    public float HP = 50;
+    [Header("攻擊力")]
+    public float Atk = 10f;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip Sound_Atk;
+    [Header("玩家")]
+    public GameObject PLAYER;
+
+
+    private void attack()
+    {
+       
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            print("殭屍攻擊");
+            print(PLAYER);
+            print("殭屍攻擊力:" + Random.Range(0f, Atk));
+        }
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attack();
+    }
+}
